ff2adcf6af6e4dc9aa1346812d133752885dca8b
Vivek Raj Srivastava <74672080+256bitguy@users.noreply.github.com>
2021-08-13 12:36:08 +0530

    https://codeforces.com/contest/104/problem/A
    [0 - 10] — zero ways.
    [11] — four ways using aces.
    [12 - 19] — four ways using cards from 2 to 9.
    [20] — 15 ways, they are described in statement.
    [21] — four ways using aces.
    [22 - 25] — zero ways.

---
+++ codeforces/800/A.Blackjack.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+ int n;
+ cin>>n;
+ if(n<=10 || n>22)
+ {
+          cout<<0;
+ }
+  
+ if(n>10 && n<=22){
+          n-=10;
+         if(n<=11){
+           switch(n) {
+                 case 1:cout<<4;
+                        break;
+                 case 2: cout<<4;
+                        break;
+                 case 3:cout<<4;
+                        break;
+                 case 4:cout<<4;
+                        break;
+                 case 5:cout<<4;
+                        break;
+                 case 6:cout<<4;
+                         break;
+                 case 7:cout<<4;
+                        break;
+                 case 8:cout<<4;
+                         break;
+                case 9:cout<<4;
+                        break;
+                case 10:cout<<15;
+                        break;
+                case 11:cout<<4;
+                break;
+                
+       }
+       }
+      }
+	return 0;
+}
